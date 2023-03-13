42f4fc3c6ce2419cd95c0a0d5164fc68f95b1aa8
Palleri <Skoog.timmy@gmail.com>
2023-03-13 15:18:53 +0100

    Fixed cron running multiple times

--- app/run.sh
+++ app/run.sh
@@ -5,16 +5,16 @@ echo "# This might take a while, it depends on how many containers are running #
 
 cd /app && tar xzvf /app/docker.tgz > /dev/null 2>&1 && cp /app/docker/* /usr/bin/ > /dev/null 2>&1
 
-
+cp  /app/root /etc/crontabs/root
 if [ -n "$CRON_TIME" ]; then
     
     hour=$(echo $CRON_TIME | grep -Po "\d*(?=:)")
     minute=$(echo $CRON_TIME | grep -Po "(?<=:)\d*")
-    echo -e "$minute  $hour   *   *   *   run-parts /etc/periodic/daily" >> /app/root
+    echo -e "$minute  $hour   *   *   *   run-parts /etc/periodic/daily" >> /etc/crontabs/root
 
     else
 
-    echo -e "30 12  *   *   *   run-parts /etc/periodic/daily" >> /app/root 
+    echo -e "30 12  *   *   *   run-parts /etc/periodic/daily" >> /etc/crontabs/root
 fi
 
 if [ "$NOTIFY" = "true" ]; then
@@ -36,12 +36,9 @@ fi
 
 
 chmod +x /app/dockcheck.sh
-cp  /app/root /etc/crontabs/root
 cp /app/dockcheck /etc/periodic/daily/dockcheck
 chmod +x /app/regctl
 chmod +x /etc/periodic/daily/dockcheck
 cp /app/regctl /usr/bin/
-
 /etc/periodic/daily/dockcheck
-
 exec crond -f
